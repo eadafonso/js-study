6f6c80e30780a29ab7c51ab8ec62f42f98ad7a70
Edvaldo Afonso <eadafonso@gmail.com>
2022-04-18 00:24:18 +0100

    feat: delete propiety object

---
+++ object/object-delete.js
@@ -0,0 +1,9 @@
+let user = {
+  name: "Jannet",
+  age: 30,
+  email: "jannet@gmail.com",
+  location: "Nigeria",
+  blogs: ["human resource management", "tech for the non-tech savvy"],
+};
+
+delete user.age;
