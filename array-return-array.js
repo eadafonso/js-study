32037625a0c1edc25262e8b444872ba6b198df18
Edvaldo Afonso <eadafonso@gmail.com>
2022-04-17 22:21:20 +0100

    feat: receive array adn return array

---
+++ array-return-array.js
@@ -0,0 +1,24 @@
+const laptop = [
+  {
+    model: "Macbook Pro",
+    price: 20000,
+  },
+  {
+    model: "Macbook Air",
+    price: 15000,
+  },
+  {
+    model: "Hp lite",
+    price: 5000,
+  },
+  {
+    model: "Dell",
+    price: 400,
+  },
+];
+
+const getLaptopModel = laptop.map((item) => {
+  return item.model;
+});
+
+console.log(getLaptopModel);
